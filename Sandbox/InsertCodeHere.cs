e5be80a73f6597b78b6a97cbfe2fbffe4945f0f8
Dshy <KortSkg@KORTSKG-PC>
2016-11-08 13:49:17 +0100

    Tiføjet manager og har brugt overrite til at få salary per month

--- Sandbox/InsertCodeHere.cs
+++ Sandbox/InsertCodeHere.cs
@@ -9,7 +9,7 @@ namespace Sandbox
         {
             // The FIRST line of code should be BELOW this line
             Worker worker1 = new Worker("Bob", 2000, "TruckDriver");
-
+            Manager manager1 = new Manager("Troy", 2000, 100, 10);
             // Add you test of the Employee class and derived classes here
 
 
---
+++ Sandbox/Manager.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Sandbox
+{
+    class Manager : Employee
+    {
+        private int monthlyBonus;
+        private int houres;
+        public Manager(string name, int salaryPerMonth, int monthlyBonus, int houres) : base(name, salaryPerMonth)
+        {
+            this.monthlyBonus = monthlyBonus;
+            this.houres = houres;
+        }
+        public override int GetSalaryPerMonth()
+        {
+            if (houres > 180)
+            {
+                monthlyBonus = 100;
+                Console.WriteLine(monthlyBonus + base.GetSalaryPerMonth());
+            }
+            else
+            {
+                Console.WriteLine("Not enouth for bonus");
+            }
+            return base.GetSalaryPerMonth();
+        }
+        //public int Bonus()
+        //{
+        //    if (monthlyBonus > 100)
+        //    {
+        //        Console.WriteLine(monthlyBonus);
+        //    }
+        //    else
+        //    {
+        //        Console.WriteLine("Not enouth bonus");
+        //    }
+        //    return monthlyBonus;
+        //}
+    }
+}

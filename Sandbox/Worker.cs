9987cb41e15dc5342a1f12cc5fd994d83f75779f
Dshy <KortSkg@KORTSKG-PC>
2016-11-08 12:58:09 +0100

    Worker class har arvet fra Employee

---
+++ Sandbox/Worker.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Sandbox
+{
+    class Worker:Employee
+    {
+        private string truckDriver;
+        private string maintenance;
+
+        public Worker(string name, int salaryPerMonth) : base(name, salaryPerMonth)
+        {
+
+        }
+    }
+}

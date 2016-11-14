78a9f9ee1101e6676299d3147605966957a27df3
Dshy <KortSkg@KORTSKG-PC>
2016-11-14 09:39:59 +0100

    Tilføet Director

---
+++ Sandbox/Director.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Sandbox
+{
+    class Director : Manager
+    {
+        public Director(string name, int salaryPerMonth, int monthlyBonus, int houres) : base(name, salaryPerMonth, monthlyBonus, houres)
+        {
+        }
+    }
+}
